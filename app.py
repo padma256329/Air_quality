b5fa7576f81788f252a9428190629abcc8917b7a
padmavathi <padmavathikora@ettma.com>
2023-10-16 12:41:01 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template, url_for
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('randomForestRegressor.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods = ['POST'])
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    print(prediction[0])
+
+    #output = round(prediction[0], 2)
+    return render_template('home.html', prediction_text="AQI for Jaipur {}".format(prediction[0]))
+
+if __name__ == '__main__':
+    app.run(debug=True)
